f412eab8807d84a379f32c0069b03cfdf9b6d328
Asim Alam Chowdhury <asimalam0032@gmail.com>
2025-08-16 16:21:46 +0600

    initial

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import ...
 public class User {
     private String username;
     private String id;
@@ -167,5 +168,44 @@ public class Student {
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+}
+
+class Appointment implements Serializable{
+    private String appointmentId;
+    private String StudentId;
+    private String authority;
+    private String date;
+    private String time;
+    private boolean isApproved;
+
+    public Appointment(String appointmentId,String studentId){
+        this.appointmentId=appointmentId;
+        this.studnetId=studentId;
+        this.authority=authority;
+        this.data=data;
+        this.time=time;
+        this.isApproved=false;
+    }
+    public String getAppointmentId(){
+        return appointmentId;
+    }
+    public String getStudentId(){
+        return studentId;
+    }
+    public String getAuthority(){
+        return authority;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getTime(){
+        return time;
+    }
+    public boolean isApproved(){
+        return isApproved;
+    }
+    public void setApproved(boolean approved){
+        this.isApproved=approved;
     }
 }
